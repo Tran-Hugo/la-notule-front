d307e601a0602322c6ea7a4405a4d553541f8b47
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2021-10-09 17:47:23 +0200

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    
+    <ul v-for="(item,key) in cartItems" :key="key" class="list-group list-group-flush">
+  <li class="list-group-item">{{item['book']['title']}}  <span class="badge bg-primary rounded-pill">{{item.quantity}}</span></li>
+</ul>
+<button type="button" @click="removeCart" class="btn btn-danger">Vider le panier</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      cartItems:[]
+    }
+  },
+  mounted(){
+    axios.get("https://127.0.0.1:8000/api/carts/1")
+        .then(data => {
+          console.log(data.data);
+          this.cartItems = data.data.cartItems
+        })
+  },
+  methods:{
+    removeCart(){
+      this.cartItems.forEach((item) => {
+  console.log(item.id) //value
+      axios.delete("https://127.0.0.1:8000/api/cart_items/"+item.id)
+          .then(res=>{console.log(res,'OK')})
+})
+    }
   }
 }
 </script>
