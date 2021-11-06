96d22f60ea0077344c4b44ae67c737c071f0e4be
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2021-11-06 15:38:25 +0100

    update cartPayment.vue

--- src/views/cart/cartPayment.vue
+++ src/views/cart/cartPayment.vue
@@ -1,118 +1,176 @@
 <template>
-{{message}}
-  <body>
-      <form class="d-flex justify-content-center" @submit.prevent="submit" id="payment-form">
-        <div class="form-row col-10 col-md-6 col-lg-6">
-            <div id="card-elements" v-on:change="cardChange(event)"></div>
-            <div id="card-errors" role="alert"></div>
-        <br>
-        <button type="button" @click.prevent="submit" class="btn btn-primary">Payer {{cartTotal}}€</button>
+  <main>
+    <h3>Paiement par carte bancaire :</h3>
+    <div class="d-flex flex-column principale">
+      <div class="my-auto">
+        <div class="cards-logos brands">
+          <img src="@/assets/mc_vrt_pos.svg" alt="master-card.svg">
+          <img src="@/assets/Visa_Brandmark_Blue_RGB_2021.svg" alt="visa.svg">
+          <img src="@/assets/AXP_BlueBoxLogo_Alternate_SMALLscale_RGB_DIGITAL_80x80.svg" alt="amex.svg">
+        </div>
+        <form
+        class="d-flex justify-content-center"
+        @submit.prevent="submit"
+        id="payment-form"
+      >
+        <div class="form-row col-12 col-md-6 col-lg-6 d-flex flex-column">
+          <div id="card-elements" v-on:change="cardChange(event)"></div>
+          <div id="card-errors" role="alert"></div>
+          <br />
+          <button type="button" @click.prevent="submit" class="btn btn-primary">
+            Payer {{ cartTotal }}€
+          </button>
         </div>
       </form>
-  </body>
+      </div>
+      
+    </div>
+    <div class="cards-logos stripe-logo-div">
+        <img class="logo-stripe" src="@/assets/Stripe-blurple.svg" alt="stripe.svg">
+    </div>
+  </main>
 </template>
 
 <script>
-import axios from 'axios'
-import {mapGetters} from 'vuex'
-import configHelper from '../../helpers/configHeader'
+import axios from "axios";
+import { mapGetters } from "vuex";
+import configHelper from "../../helpers/configHeader";
 
-let stripe = window.Stripe('pk_test_51JinhbIWZlqy6WjcZXGkpfEZhzIxhrVBvtHJpQTC8dUZ2fBM51VHUJ83Ow6je0sM4tV1RJlRk5tTlkquPN3J3lP800Y24MdTrk'),
-    elements = stripe.elements(),
-    card = undefined;
+let stripe = window.Stripe(
+    "pk_test_51JinhbIWZlqy6WjcZXGkpfEZhzIxhrVBvtHJpQTC8dUZ2fBM51VHUJ83Ow6je0sM4tV1RJlRk5tTlkquPN3J3lP800Y24MdTrk"
+  ),
+  elements = stripe.elements(),
+  card = undefined;
 export default {
-    name:'cartPayment',
-    data(){
-        return {
-            intentSecret:'',
-            cartId:'',
-            cardholderName:'',
-            cardholderEmail:'',
-            message:'',
-            cartTotal:''
+  name: "cartPayment",
+  data() {
+    return {
+      intentSecret: "",
+      cartId: "",
+      cardholderName: "",
+      cardholderEmail: "",
+      message: "",
+      cartTotal: "",
+    };
+  },
+  mounted() {
+    card = elements.create("card",{
+        style: {
+            base: {
+                fontSize: '18px'
+            }
         }
-    },
-    mounted(){
-        card = elements.create('card');
-        card.mount('#card-elements');
-        card.addEventListener("change", (event)=>{
-             console.log(event)
-            let displayError = document.getElementById('card-errors');
-            if(event.error){
-                displayError.textContent = event.error.message;
-            } else {
-                displayError.textContent = "";
-            }
+    });
+    card.mount("#card-elements");
+    card.addEventListener("change", (event) => {
+      console.log(event);
+      let displayError = document.getElementById("card-errors");
+      if (event.error) {
+        displayError.textContent = event.error.message;
+      } else {
+        displayError.textContent = "";
+      }
+    });
+    axios
+      .get(configHelper.domain + "/payment/" + this.cart, configHelper.config())
+      .then((res) => {
+        console.log(res.data);
+        this.intentSecret = res.data.intentSecret;
+        this.cartId = res.data.cart["id"];
+        this.cardholderName = res.data.user.lastname;
+        this.cardholderEmail = res.data.user.email;
+        this.cartTotal = res.data.cart.total;
+      });
+  },
+  beforeUnmount() {
+    card.destroy("#card-elements");
+  },
+  methods: {
+    submit() {
+      stripe
+        .handleCardPayment(this.intentSecret, card, {
+          payment_method_data: {
+            billing_details: {
+              name: this.cardholderName,
+              email: this.cardholderEmail,
+            },
+          },
         })
-        axios.get(configHelper.domain+"/payment/"+this.cart,configHelper.config())
-            .then(res=>{
-                console.log(res.data)
-                this.intentSecret = res.data.intentSecret
-                this.cartId = res.data.cart['id']  
-                this.cardholderName = res.data.user.lastname
-                this.cardholderEmail = res.data.user.email
-                this.cartTotal = res.data.cart.total
-            })
-    },
-    beforeUnmount() {
-      card.destroy('#card-elements');
-    },
-    methods: {
-        submit(){
-            stripe.handleCardPayment(
-                this.intentSecret,
-                card,
-                {
-                    payment_method_data : {
-                        billing_details:{
-                            name: this.cardholderName,
-                            email:this.cardholderEmail
-                        }
-                    }
-                }
-            ).then((result)=>{
-                if('paymentIntent' in result){
-                    // console.log(result.paymentIntent,result)
-                    // this.stripeTokenHandler(result.paymentIntent)
-                    
-                let data = {
-                'stripeIntentId':result.paymentIntent.id,
-                'stripeIntentPaymentMethod':result.paymentIntent.payment_method,
-                'stripeIntentStatus':result.paymentIntent.status,
-                'subscription':this.cartId
-            }
-            console.log(data)
-             axios.post(configHelper.domain+'/payment/'+this.cartId+'/subscription',data)
-                    return 
-                } else if ('error' in result) {
-                    console.log(result.error.code)
-                    this.message = 'Carte refusée'
-                }
-            })
-             .then(()=>window.location.href="http://localhost:8080/#")
-        },
-        // async stripeTokenHandler(intent){
-        //     let data = {
-        //         'stripeIntentId':intent.id,
-        //         'stripeIntentPaymentMethod':intent.payment_method,
-        //         'stripeIntentStatus':intent.status,
-        //         'subscription':this.cartId
-        //     }
-        //     console.log(data)
-        //     await axios.post('https://127.0.0.1:8000/payment/'+this.cartId+'/subscription',data)
-        //     // card.clear();
-        //             .then(setTimeout(window.location.href="http://localhost:8080/",4000))
-        // }
-    },
-    computed: {
-    ...mapGetters({
-        cart:'getCart',
-        })
-  }
+        .then((result) => {
+          if ("paymentIntent" in result) {
+            // console.log(result.paymentIntent,result)
+            // this.stripeTokenHandler(result.paymentIntent)
 
-}
+            let data = {
+              stripeIntentId: result.paymentIntent.id,
+              stripeIntentPaymentMethod: result.paymentIntent.payment_method,
+              stripeIntentStatus: result.paymentIntent.status,
+              subscription: this.cartId,
+            };
+            console.log(data);
+            axios.post(
+              configHelper.domain + "/payment/" + this.cartId + "/subscription",
+              data
+            );
+            return;
+          } else if ("error" in result) {
+            console.log(result.error.code);
+            this.message = "Carte refusée";
+          }
+        })
+        .then(() => (window.location.href = "http://localhost:8080/#"));
+    },
+    // async stripeTokenHandler(intent){
+    //     let data = {
+    //         'stripeIntentId':intent.id,
+    //         'stripeIntentPaymentMethod':intent.payment_method,
+    //         'stripeIntentStatus':intent.status,
+    //         'subscription':this.cartId
+    //     }
+    //     console.log(data)
+    //     await axios.post('https://127.0.0.1:8000/payment/'+this.cartId+'/subscription',data)
+    //     // card.clear();
+    //             .then(setTimeout(window.location.href="http://localhost:8080/",4000))
+    // }
+  },
+  computed: {
+    ...mapGetters({
+      cart: "getCart",
+    }),
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.principale {
+  min-height: 26rem;
+  border: 1px solid wheat;
+  margin: 1rem;
+}
+.cards-logos img{
+  height: 3rem;
+}
+.brands{
+  padding: 1rem;
+  display: flex;
+  justify-content: flex-end;
+}
+.logo-stripe{
+  height: 2rem !important;
+}
+.stripe-logo-div{
+  display: flex;
+  justify-content: flex-end;
+  padding: 0 1rem;
+}
+@media screen and (min-width: 768px) {
+  .principale{
+    min-height: 36rem;
+  }
+}
+@media screen and (min-width: 992px) {
+  main{
+    padding: 9rem 4rem 0 4rem;
+  }
+}
 </style>
