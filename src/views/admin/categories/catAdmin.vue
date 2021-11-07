a92b27fe8f89e4ff33b30ae27af04e32fa720de9
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2021-11-07 20:53:02 +0100

    Update catAdmin.vue

--- src/views/admin/categories/catAdmin.vue
+++ src/views/admin/categories/catAdmin.vue
@@ -1,7 +1,7 @@
 <template>
   <h4 class="text-center p-2">Catégories</h4>
   <div class="d-flex justify-content-end col-10">
-    <router-link :to="{ name: 'addCategory'}"><button type="button" class="btn btn-primary me-2">Ajouter une Catégorie</button></router-link>
+    <router-link :to="{ name: 'addCategory'}"><button type="button" class="btn btn-turquoise me-2">Ajouter une Catégorie</button></router-link>
   </div>
   <br>
         <div class="d-flex justify-content-center">
@@ -22,12 +22,12 @@
                         <td>{{category.name}}</td>
                         <td>
                             <router-link :to="{ name: 'category', params: { id: category.id }}">
-                                <button type="button" class="btn btn-success me-2">voir</button>
+                                <button type="button" class="btn btn-pastel-green me-2">voir</button>
                             </router-link>
                             <router-link :to="{ name: 'editCat', params: { id: category.id }}">
-                                <button type="button" class="btn btn-warning me-2">éditer</button>
+                                <button type="button" class="btn btn-pastel-yellow me-2">éditer</button>
                             </router-link>
-                            <button type="button" @click="remove(category.id)" class="btn btn-danger me-2">Supprimer</button>
+                            <button type="button" @click="remove(category.id)" class="btn btn-pastel-red me-2">Supprimer</button>
                         </td>
                         </tr>
                     </tbody>
@@ -69,5 +69,6 @@ export default {
 </script>
 
 <style>
+@import '../../../assets/css/btn-colors.css';
 
 </style>
