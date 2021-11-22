74f2a1e98384c7d0bfe5e377ea28bd023b4c5ffa
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2021-11-22 14:49:42 +0100

    Update interceptors.js

--- src/services/interceptors.js
+++ src/services/interceptors.js
@@ -1,36 +1,40 @@
-import axios from 'axios'
-import store from '../store'
-import router from '../router';
+import axios from "axios";
+import store from "../store";
+import router from "../router";
+import configHelper from "../helpers/configHeader";
 
-export default
-() =>{
-    axios.interceptors.response.use(
-    res => {
-        return res
+export default () => {
+  axios.interceptors.response.use(
+    (res) => {
+      return res;
     },
-    err=> {
-        const originalConfig = err.config;
-        if(originalConfig.url !== "https://127.0.0.1:8000/api/login") {
-            if (err.response.status === 401 && err.response.data.message === "Expired JWT Token") {
-                // console.log("easy")
-                let refreshToken = {
-                    refresh_token: store.state.loginModule.refreshToken
-                }
-                axios.post("https://127.0.0.1:8000/api/token/refresh", refreshToken)
-                    .then(res=>{
-                        console.log(res)
-                        store.commit('SET_TOKEN',res.data.token)
-                        localStorage.setItem('token',res.data.token)
-                        if(router.currentRoute._value.fullPath.substring(0,6)=="/admin"){
-                            alert('veuillez cliquer à nouveau s\'il vous plait')
-                        } else {
-                            // router.push(store.getters.getPath)
-                            router.push({name:store.getters.getPath})
-                        }
-                    })
-            }
+    (err) => {
+      const originalConfig = err.config;
+      if (originalConfig.url !== configHelper.domain + "/api/login") {
+        if (
+          err.response.status === 401 &&
+          err.response.data.message === "Expired JWT Token"
+        ) {
+          let refreshToken = {
+            refresh_token: store.state.loginModule.refreshToken,
+          };
+          axios
+            .post(configHelper.domain + "/api/token/refresh", refreshToken)
+            .then((res) => {
+              console.log(res);
+              store.commit("SET_TOKEN", res.data.token);
+              localStorage.setItem("token", res.data.token);
+              if (
+                router.currentRoute._value.fullPath.substring(0, 6) == "/admin"
+              ) {
+                alert("veuillez cliquer à nouveau s'il vous plait");
+              } else {
+                router.push({ name: store.getters.getPath });
+              }
+            });
         }
-        return Promise.reject(err)
+      }
+      return Promise.reject(err);
     }
-)
-}
+  );
+};
